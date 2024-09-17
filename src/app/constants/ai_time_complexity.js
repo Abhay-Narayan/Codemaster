cb33842f2671337ec460e354dad1a0099435f15c
AbhayETH <abhaynarayan16122002@gmail.com>
2024-09-17 14:07:23 +0530

    removed console logs

--- src/app/constants/ai_time_complexity.js
+++ src/app/constants/ai_time_complexity.js
@@ -4,7 +4,7 @@ const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export const getComplexity=async(code)=>{
-    console.log(process.env.GOOGLE_API)
+    
     const prompt=`identify the time complexity of below code and just return with the time complexity and space complexity nothing else Code- ${code} dont explain anything `;
     const res=await model.generateContent(prompt);
     console.log(res.response.text());
